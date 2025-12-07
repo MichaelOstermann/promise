be66523513bfdfc471c659e50c03df88c248f954
Michael Ostermann <michaelostermann@me.com>
2025-12-07 12:09:57 +0100

    chore: migrations

--- packages/unplugin-promise/src/index.ts
+++ packages/unplugin-promise/src/index.ts
@@ -25,7 +25,7 @@ export default createUnplugin<Options>(({ debug, enforce, exclude, include } = {
                     debug: shouldDebug(id),
                     resolve({ importAlias, importName, importPath, propertyName }) {
                         if (importPath === "@monstermann/promise" && importName === "Promise")
-                            return `import { ${propertyName} as ${importAlias} } from "@monstermann/promise/Promise/${propertyName}.js"`
+                            return `import { ${propertyName} as ${importAlias} } from "@monstermann/promise/Promise/${propertyName}.mjs"`
                         return undefined
                     },
                 })
