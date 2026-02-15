01930973901bf74c88051ba9ba33c671da19b254
Michael Ostermann <michaelostermann@me.com>
2026-02-15 14:21:23 +0100

    feat!: improve limit types

--- docs/Promise/limit.md
+++ docs/Promise/limit.md
@@ -1,27 +1,27 @@
 # limit
 
 ```ts
-function Promise.limit(...args: T): Promise<Awaited<U>>
+function Promise.limit(options: number | { concurrency: number }): Limited
 ```
 
-Limits the concurrency of function execution.
+Creates a limiter that controls the concurrency of function execution.
 
 ## Example
 
 ```ts
 import { Promise } from "@monstermann/promise";
 
-const limitedFetch = Promise.limit(fetch, { concurrency: 3 });
-const limitedFetch2 = Promise.limit(fetch, 3); // Shorthand
+const limiter = Promise.limit({ concurrency: 3 });
+const limiter2 = Promise.limit(3); // Shorthand
 
 const results = await Promise.all([
-    // At most 3 fetch calls are executed at any time
-    limitedFetch("/api/1"),
-    limitedFetch("/api/2"),
-    limitedFetch("/api/3"),
-    limitedFetch("/api/4"),
+    // At most 3 functions are executed at any time
+    limiter(() => fetch("/api/1")),
+    limiter(() => fetch("/api/2")),
+    limiter(() => fetch("/api/3")),
+    limiter(() => fetch("/api/4")),
 ]);
 
 // Wait for queue to become idle
-await limitedFetch.idle();
+await limiter.idle();
 ```
--- packages/promise/src/Promise/limit.ts
+++ packages/promise/src/Promise/limit.ts
@@ -1,7 +1,7 @@
 import { queue } from "./queue"
 
-export interface Limited<T extends unknown[], U> {
-    (...args: T): Promise<Awaited<U>>
+export interface Limited {
+    <T>(fn: () => Promise<T>): Promise<Awaited<T>>
     idle: () => Promise<void>
 }
 
@@ -13,40 +13,39 @@ type LimitOptions = number | {
  * # limit
  *
  * ```ts
- * function Promise.limit(...args: T): Promise<Awaited<U>>
+ * function Promise.limit(options: number | { concurrency: number }): Limited
  * ```
  *
- * Limits the concurrency of function execution.
+ * Creates a limiter that controls the concurrency of function execution.
  *
  * ## Example
  *
  * ```ts
  * import { Promise } from "@monstermann/promise";
  *
- * const limitedFetch = Promise.limit(fetch, { concurrency: 3 });
- * const limitedFetch2 = Promise.limit(fetch, 3); // Shorthand
+ * const limiter = Promise.limit({ concurrency: 3 });
+ * const limiter2 = Promise.limit(3); // Shorthand
  *
  * const results = await Promise.all([
- *     // At most 3 fetch calls are executed at any time
- *     limitedFetch("/api/1"),
- *     limitedFetch("/api/2"),
- *     limitedFetch("/api/3"),
- *     limitedFetch("/api/4"),
+ *     // At most 3 functions are executed at any time
+ *     limiter(() => fetch("/api/1")),
+ *     limiter(() => fetch("/api/2")),
+ *     limiter(() => fetch("/api/3")),
+ *     limiter(() => fetch("/api/4")),
  * ]);
  *
  * // Wait for queue to become idle
- * await limitedFetch.idle();
+ * await limiter.idle();
  * ```
  *
  */
-export function limit<T extends unknown[], U>(
-    fn: (...args: T) => U,
+export function limit(
     options: LimitOptions,
-): Limited<T, U> {
+): Limited {
     const q = queue(options)
 
-    const limited: Limited<T, U> = function (...args) {
-        return q.add(() => fn(...args))
+    const limited: Limited = function (fn) {
+        return q.add(() => fn())
     }
 
     limited.idle = q.idle
